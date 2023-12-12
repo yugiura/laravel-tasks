4fa9144c7648182f43a23ff3a9d83d8e8836e274
sugiura <sugiura@winas.jp>
2023-12-12 09:17:30 +0900

    test commit and release

--- config/view.php
+++ config/view.php
@@ -10,7 +10,7 @@ return [
     | Most templating systems load templates from disk. Here you may specify
     | an array of paths that should be checked for your views. Of course
     | the usual Laravel view path has already been registered for you.
-    |
+    | commit test
     */
 
     'paths' => [
