f4cf88f5a61b603f1cdde493eb6a5350232171af
sugiura <sugiura@winas.jp>
2023-12-13 09:01:08 +0900

    change database name

--- config/database.php
+++ config/database.php
@@ -48,7 +48,7 @@ return [
             'url' => env('DATABASE_URL'),
             'host' => env('sugiuralaravelonappservice-server.mysql.database.azure.com', '127.0.0.1'),
             'port' => env('DB_PORT', '3306'),
-            'database' => env('DB_DATABASE', 'forge'),
+            'database' => env('sugiuralaravelonappservice-database', 'forge'),
             'username' => env('jiidssaqxz', 'forge'),
             'password' => env('88XXZ64TC455V2UA$', ''),
             'unix_socket' => env('DB_SOCKET', ''),
